be19caacd5172f1b4ed8d979f616604984876e6d
VICTOR LUIZ SILVA SANTOS <victor.santos8@alunos.sc.senac.br>
2023-11-09 19:33:33 -0300

    inicio base projeto

---
+++ MoonlightApp/src/main/java/com/moonlight/moonlightapp/MoonlightApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.moonlight.moonlightapp;
+
+/**
+ *
+ * @author amanda.medeiros1
+ */
+public class MoonlightApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
