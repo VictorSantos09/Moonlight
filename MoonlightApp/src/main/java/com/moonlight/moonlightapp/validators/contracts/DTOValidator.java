b983f7811c5b05277b84d7fef9e2c21d5da00ac7
VICTOR LUIZ SILVA SANTOS <victor.santos8@alunos.sc.senac.br>
2023-11-16 19:56:25 -0300

    imp implementado service para busca dos produtos
    - fornece simplicidade para a busca das informações
    - removido imports não utilizados

--- MoonlightApp/src/main/java/com/moonlight/moonlightapp/validators/contracts/DTOValidator.java
+++ MoonlightApp/src/main/java/com/moonlight/moonlightapp/validators/contracts/DTOValidator.java
@@ -3,9 +3,6 @@ package com.moonlight.moonlightapp.validators.contracts;
 import com.moonlight.moonlightapp.dtos.DTO;
 import com.moonlight.moonlightapp.validators.ResultadoValidacao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface DTOValidator<T extends DTO> {
     ResultadoValidacao validar(T dto);
 }
