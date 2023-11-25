8ef9ab3f44ae38e73d7579055446b8f4fd8fcf8f
VICTOR LUIZ SILVA SANTOS <victor.santos8@alunos.sc.senac.br>
2023-11-25 01:42:01 -0300

    criado menu principal
    - esta conectado as funcionalidades existentes do produto

--- MoonlightApp/src/main/java/com/moonlight/moonlightapp/MoonlightApp.java
+++ MoonlightApp/src/main/java/com/moonlight/moonlightapp/MoonlightApp.java
@@ -6,6 +6,7 @@ package com.moonlight.moonlightapp;
 
 import com.moonlight.moonlightapp.daos.ConexaoBanco;
 import com.moonlight.moonlightapp.services.processos.RemoverProcessoService;
+import com.moonlight.moonlightapp.views.principal.MenuPrincipalView;
 import com.moonlight.moonlightapp.views.produtos.CriarProdutoView;
 import com.moonlight.moonlightapp.views.produtos.ProdutoPrincipalView;
 
@@ -21,7 +22,7 @@ public class MoonlightApp {
         if (!isConexaoOk) {
             JOptionPane.showMessageDialog(null, "Falha ao conectar ao banco de dados");
         }
-        ProdutoPrincipalView view = new ProdutoPrincipalView();
+        MenuPrincipalView view = new MenuPrincipalView();
         view.setVisible(true);
     }
 }
