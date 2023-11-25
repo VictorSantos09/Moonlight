70c5d6e4c7608680532d95cbc28e30c3fd49c12a
AMANDA HIRATA MEDEIROS <amanda.medeiros1@alunos.sc.senac.br>
2023-11-24 21:54:08 -0300

    adição da classe de criação de matéria prima

---
+++ MoonlightApp/src/main/java/com/moonlight/moonlightapp/views/materiaprimas/CriarMateriaPrima.java
@@ -0,0 +1,23 @@
+package com.moonlight.moonlightapp.views.materiaprimas;
+
+import com.moonlight.moonlightapp.daos.TipoMateriaPrimaDAO;
+import com.moonlight.moonlightapp.daos.UnidadeMedidaDAO;
+import com.moonlight.moonlightapp.dtos.materiasprimas.MateriaPrimaDTO;
+import com.moonlight.moonlightapp.dtos.unidadesmedidas.UnidadeMedidaDTO;
+
+
+public class CriarMateriaPrima {
+    
+    private UnidadeMedidaDAO unidadeMedidaDAO = new UnidadeMedidaDAO();
+    private TipoMateriaPrimaDAO tipoMateriaPrimaDAO = new TipoMateriaPrimaDAO();
+        
+    public void CriarMateriaPrima(MateriaPrimaDTO dto){     
+        
+        
+        
+    
+        
+    
+    }
+    
+}
